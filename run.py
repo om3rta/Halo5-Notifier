7f2493b3de39ce90e2192472a02218d0c01aa429
om3rta <spamitycalamity@gmail.com>
2015-11-28 00:16:09 -0500

    Changed so that user is prompted for palyer name. Also prompts for api_key on first use and remembers for future uses by writing the api_key to a file

--- run.py
+++ run.py
@@ -3,17 +3,43 @@ from GrabStats import GrabStats
 from BuildNotification import BuildNotification
 import time
 import sys
+import os
 
-user_name = sys.argv[1]
+user_name = raw_input("Track games for which user?: ")
+
+
+#get the api key
 api_key = ""
+key_location = os.getcwd() + "/apikey.txt"
+if os.path.isfile(key_location) == True:
+	open_file = open(key_location, "r")
+	api_key = open_file.read()
+	grab = GrabStats()
+	stats = grab.last_game_info(user_name, api_key)
+	notify = BuildNotification()
+	last_playlist = grab.playlist_stats(stats["PlaylistId"], user_name, api_key)
+else:
+	key_ready = False
+	while key_ready == False:
+		print "\nApi key missing or incorrect."
+		api_key = raw_input("Enter your api key: ")
+		print "Let me check that right quick..."
+		try:
+			grab = GrabStats()
+			stats = grab.last_game_info(user_name, api_key)
+			notify = BuildNotification()
+			last_playlist = grab.playlist_stats(stats["PlaylistId"], user_name, api_key)
+			api_key_file = open("apikey.txt", "w+")
+			api_key_file.write(api_key)
+			api_key_file.close()
+			print "\nApi key recorded. All set."
+			key_ready = True
+		except:
+			pass
 
-grab = GrabStats()
-stats = grab.last_game_info(user_name, api_key)
-notify = BuildNotification()
+#api_key = "ecbe0ddbe57246c4bf82cd0f4f3be301"
 
-last_playlist = grab.playlist_stats(stats["PlaylistId"], user_name, api_key)
-
-icon = str(last_playlist["designation"]) + str(last_playlist["tier"]) + ".png"
+#Send first notification
 if last_playlist["designation_id"] < 6:
 	rank = last_playlist["designation"] + " " + str(last_playlist["tier"])
 	progress = last_playlist["progress"]
@@ -24,12 +50,13 @@ else:
 	progress = last_playlist["csr"]
 	icon = str(last_playlist["designation"]) + ".png"
 	onyxchamp = True
-
 send_notification = notify.send_notification(stats["PlaylistName"], icon, progress, user_name, rank, onyxchamp)
 
+#Notification loop
 current_id = stats["PlaylistId"]
 new_id = stats["PlaylistId"]
 keepalive = True
+
 while keepalive == True:
 	while new_id == current_id:
 		time.sleep(5)
@@ -39,4 +66,15 @@ while keepalive == True:
 	current_id = new_id
 	stats = grab.last_game_info(user_name, api_key)	
 	last_playlist = grab.playlist_stats(stats["PlaylistId"], user_name, api_key)
-	send_notification = notify.send_notification(stats["PlaylistName"], icon, last_playlist["progress"], user_name, rank)
+	
+	if last_playlist["designation_id"] < 6:
+		rank = last_playlist["designation"] + " " + str(last_playlist["tier"])
+		progress = last_playlist["progress"]
+		icon = str(last_playlist["designation"]) + str(last_playlist["tier"]) + ".png"
+		onyxchamp = False
+	else:
+		rank = last_playlist["designation"]
+		progress = last_playlist["csr"]
+		icon = str(last_playlist["designation"]) + ".png"
+		onyxchamp = True
+		send_notification = notify.send_notification(stats["PlaylistName"], icon, last_playlist["progress"], user_name, rank)
